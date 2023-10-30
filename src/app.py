975aa1164856e2228d21fc43c77cc57d6f260b02
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2023-10-30 17:11:12 -0400

    add pages file

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-from src.form.form import TransactionForm, TransactionsCsvForm
+from src.pages import HomePage, DataEntryPage
+
+PAGES = [HomePage, DataEntryPage]  # first page is the default page
 
 
 class BudgetApp(tk.Tk):
@@ -12,46 +14,16 @@ class BudgetApp(tk.Tk):
 
         self.frames = {}
 
-        for Page in (HomePage, DataEntryPage):
+        for Page in PAGES:
             frame = Page(container, self)
             self.frames[Page] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(HomePage)
+        self.show_frame(PAGES[0])
 
     def show_frame(self, frame_class):
         frame = self.frames[frame_class]
         frame.tkraise()
-
-
-class DataEntryPage(tk.Frame):
-    def __init__(self, parent, controller):
-        super().__init__(parent)
-        label = tk.Label(self, text="Data Entry Page")
-        label.pack(pady=10, padx=10)
-        button = tk.Button(
-            self,
-            text="Go to Home",
-            command=lambda: controller.show_frame(HomePage),
-        )
-        button.pack()
-        TransactionForm(self)
-        TransactionsCsvForm(self)
-
-
-class HomePage(tk.Frame):
-    def __init__(self, parent, controller):
-        super().__init__(parent)
-        label = tk.Label(self, text="Home Page")
-        label.pack(pady=10, padx=10)
-        button = tk.Button(
-            self,
-            text="Go to Data Entry Page",
-            command=lambda: controller.show_frame(DataEntryPage),
-        )
-        button.pack()
-        TransactionForm(self)
-        TransactionsCsvForm(self)
 
 
 def run():
